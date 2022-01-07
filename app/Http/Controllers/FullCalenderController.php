096d573422fcff658a34b1396579b61e98c2b93d
Jakub Dakowski <jakubdakowski@gmail.com>
2022-01-07 15:55:58 +0100

    add abort message

--- app/Http/Controllers/FullCalenderController.php
+++ app/Http/Controllers/FullCalenderController.php
@@ -10,29 +10,27 @@ use App\Models\Event;
 class FullCalenderController extends Controller
 {
 	public function index(Request $request)
-    {
-      /*
+	{
+		/*
       GET
       */
-    	if($request->ajax())
-    	{
+		if ($request->ajax()) {
 			$found = Event::join('attendances', 'events.id', '=', 'attendances.event_id')
-						->where('user_id', '=', Auth::id());
-    		$data = $found->whereDate('start', '>=', $request->start)
-                       ->whereDate('end',   '<=', $request->end)
-                       ->get(['id', 'title', 'start', 'end']);
-            return response()->json($data);
-    	}
-    	return view('full-calender');
-    }
+				->where('user_id', '=', Auth::id());
+			$data = $found->whereDate('start', '>=', $request->start)
+				->whereDate('end',   '<=', $request->end)
+				->get(['id', 'title', 'start', 'end']);
+			return response()->json($data);
+		}
+		return view('full-calender');
+	}
 
-    public function action(Request $request)
-    /*
+	public function action(Request $request)
+	/*
     Requesty POST (pozostała tylko aktualizacja terminu)
     */
-    {
-    	if($request->ajax())
-    	{
+	{
+		if ($request->ajax()) {
 			$events = Event::join('attendances', 'events.id', '=', 'attendances.event_id')->where('attendances.user_id', '=', Auth::id())->where('events.id', $request->id);
 			if ($events->where('events.id', $request->id)->count() > 0) {
 				if ($events->first()->is_admin) {
@@ -42,12 +40,11 @@ class FullCalenderController extends Controller
 					]);
 					return response();
 				} else {
-					return abort(405);
+					return abort(405, "You're not the administrator of this event");  // Brak uprawnień
 				}
 			} else {
-				return abort(401);
+				return abort(401, "You don't have access to any such event"); // Nie znaleziono wydarzenia
 			}
-    	}
-    }
+		}
+	}
 }
-?>
