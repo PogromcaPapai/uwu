0cff5a49336098643214eb6f0f1293682f95bd85
Jakub Dakowski <jakubdakowski@gmail.com>
2022-10-25 13:58:59 +0200

    fix routes

--- services/main.py
+++ services/main.py
@@ -6,16 +6,15 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from libs.weather import get_alerts, forecast
 from models import Preference
-def generate_tips(forecast):
-    pass
+from uvicorn import run
 
 app = FastAPI()
 
-app.get("/services/alerts/{voivodeship}")
+@app.get("/services/alerts/{voivodeship}")
 async def _alerts(voivodeship: str, type_: str = "meteo"):
     return get_alerts(voivodeship, type_)
 
-app.get("/services/forecast/{lat}-{lon}/{moment}")
+@app.get("/services/forecast/{lat}-{lon}/{moment}")
 async def _forecast(lat: float, lon: float, moment: datetime):
     return forecast(lat, lon, moment)
 
@@ -28,5 +27,3 @@ def forecast_for_user(user:int, lat: float, lon: float, moment: datetime):
     # alerts = {
         
     # }
-    
-    
