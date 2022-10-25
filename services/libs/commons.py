08716e85f21bfccbfaf2e15b2e3a9df47cf11a44
Jakub Dakowski <jakubdakowski@gmail.com>
2022-10-25 15:33:19 +0200

    xD

--- services/libs/commons.py
+++ services/libs/commons.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from json import load as json_load
 from pathlib import Path
 
-with open(Path(__file__).parent.parent.with_name('config.json')) as f:
+with open(Path(__file__).parent.with_name('config.json')) as f:
     CONFIG = json_load(f)
 
 def middle(a: datetime, b: datetime):
