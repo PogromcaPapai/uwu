6bb859e3f6af27d9b69c743b5fa49f0a52d8c757
Jakub Dakowski <jakubdakowski@gmail.com>
2022-10-27 02:38:54 +0200

    transfer getter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,6 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/getter.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
